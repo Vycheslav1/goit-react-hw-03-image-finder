4556ee0cbddcbff55851719b1a6c1f5f02218224
Vyacheslav Stetsenko <slavastetsenko@gmail.com>
2023-08-29 21:45:17 +0300

    files

--- src/components/PicturesSearch/PicturesSearch.js
+++ src/components/PicturesSearch/PicturesSearch.js
@@ -20,7 +20,7 @@ class PicturesSearch extends Component {
   state = {
     pictures: [],
     q: '',
-    page: 1,
+    page: 0,
     show: false,
     isLoading: false,
     showButton: false,
@@ -29,7 +29,7 @@ class PicturesSearch extends Component {
   items = this.state;
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.state.q !== prevState.q || this.state.page !== prevState.page) {
+    if (this.state.q !== prevState.q || this.state.page !== this.items.page) {
       getPicturesGallery(this.state.q, this.state.page)
         .then(response => {
           this.items.pictures = [...this.items.pictures, ...response.data.hits];
@@ -75,7 +75,7 @@ class PicturesSearch extends Component {
     });
 
     this.items.pictures = [];
-    this.items.page = 1;
+    this.items.page += 1;
   };
 
   handleChangePage = () => {
