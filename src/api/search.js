8a01de7110a301f67b339fdad9a9913ec64f4906
Vyacheslav Stetsenko <slavastetsenko@gmail.com>
2023-08-29 17:08:26 +0300

    files

--- src/api/search.js
+++ src/api/search.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+const baseURL = 'https://pixabay.com/api/';
 
 /*async function getPicturesGallery(query, pageNumber) {
   const response = await axios.get(axios.defaults.baseURL, {
@@ -16,11 +16,11 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 
   return response;
 }*/
-const getPicturesGallery = async (query, pageNumber) => {
-  const results = await axios.get(axios.defaults.baseURL, {
+const getPicturesGallery = async items => {
+  const results = await axios.get(baseURL, {
     params: {
-      q: query,
-      page: pageNumber,
+      q: items.q,
+      page: items.p,
       key: '38043357-f10dc93754f8f78d0f9509fe0',
       image_type: 'photo',
       orientation: 'horizontal',
