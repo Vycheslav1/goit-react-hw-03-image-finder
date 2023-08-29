e4b7b9fe95044c6ebf3acc9c763704b75a717a98
Vyacheslav Stetsenko <slavastetsenko@gmail.com>
2023-08-29 20:21:45 +0300

    files

--- src/api/search.js
+++ src/api/search.js
@@ -6,7 +6,7 @@ async function getPicturesGallery(query, pageNumber) {
   const response = await axios.get(axios.defaults.baseURL, {
     params: {
       q: query,
-      page: 2,
+      page: pageNumber,
       key: '38043357-f10dc93754f8f78d0f9509fe0',
       image_type: 'photo',
       orientation: 'horizontal',
